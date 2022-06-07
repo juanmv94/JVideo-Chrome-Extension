5f3a1f7b29835f64be0e96925dab64df754acd61
juanmv94 <juanmv94@hotmail.com>
2022-06-07 02:35:03 +0200

    Manifest v3

--- popup.js
+++ popup.js
@@ -6,39 +6,28 @@ var texto = document.getElementById('texto');
 var vidurl;
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'try{document.getElementsByTagName("video")[0].currentSrc;} catch(err){\
-		var iframes=document.getElementsByTagName("iframe");\
-		for (let i=0; i<iframes.length;i++) {try{iframes[i].contentWindow.document.getElementsByTagName("video")[0].currentSrc; break;} catch(err){}}\
-		\}'},
-		function(result){
-			vidurl=result[0];
-			if (vidurl!=null && vidurl!="") {
-				if (vidurl.slice(0,4)=="blob")
-				{
-					texto.innerHTML="Video incompatible";
-				}
-				else
-				{
-					texto.innerHTML="¡Video detectado!";
-					texto.style.color="green";
-					ver.disabled=false;
-					descargar.disabled=false;
-				}
+	chrome.scripting.executeScript({target: {tabId: tabs[0].id}, func: ()=>{
+		try {return document.getElementsByTagName("video")[0].currentSrc;} catch(err) {
+			for (let ifr of document.getElementsByTagName("iframe")) {
+				try {return ifr.contentWindow.document.getElementsByTagName("video")[0].currentSrc;} catch(err) {}
 			}
-		});
-  });
+		}
+	}, args: []}, result => {
+		vidurl=result[0].result;
+		if (vidurl!=null && vidurl!="") {
+			if (vidurl.startsWith("blob")) texto.innerHTML="Video incompatible";
+			else {
+				texto.innerHTML="¡Video detectado!";
+				texto.style.color="green";
+				ver.disabled=false;
+				descargar.disabled=false;
+			}
+		}
+	});
+});
 
 ver.onclick = function(element) {
 if (vidurl==null && vidurl!="") return;
-/*
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'window.open("'+vidurl+'","_blank");'});
-  });
-  */
   chrome.tabs.create({url:vidurl},null);
   window.close();
 };
